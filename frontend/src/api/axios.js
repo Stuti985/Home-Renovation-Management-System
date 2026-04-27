6874806c5f9f93a764d981e930ff998c8567ac70
stuti <stuti@local>
2026-04-28 02:11:33 +0530

    Debug MongoDB connection

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://home-renovation-management-system.onrender.com/api"
-});
+baseURL: import.meta.env.VITE_API_URL});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
